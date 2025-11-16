b3f7fca140cfd80219761547a01e91ff4f95c5af
jeukengange-beep <jeukengange@gmail.com>
2025-11-16 11:48:01 +0100

    Add new gamme and terrain gallery for Immoplus

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ImmoplusLayout from "./pages/immoplus/ImmoplusLayout";
+import ImmoplusHome from "./pages/immoplus/ImmoplusHome";
+import ImmoplusAbout from "./pages/immoplus/ImmoplusAbout";
+import ImmoplusProperties from "./pages/immoplus/ImmoplusProperties";
+import ImmoplusServices from "./pages/immoplus/ImmoplusServices";
+import ImmoplusContact from "./pages/immoplus/ImmoplusContact";
 import Index from "./pages/Index";
 import Preview from "./pages/Preview";
 import GeneratedSite from "./pages/GeneratedSite";
@@ -17,7 +23,14 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<ImmoplusLayout />}>
+            <Route index element={<ImmoplusHome />} />
+            <Route path="nos-biens" element={<ImmoplusProperties />} />
+            <Route path="nos-services" element={<ImmoplusServices />} />
+            <Route path="a-propos" element={<ImmoplusAbout />} />
+            <Route path="contact" element={<ImmoplusContact />} />
+          </Route>
+          <Route path="/factory" element={<Index />} />
           <Route path="/preview/:slug" element={<Preview />} />
           <Route path="/site/:slug" element={<GeneratedSite />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
